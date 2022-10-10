6478c712dfa8577afcd7b9c8910d8bd3f81a90a2
Joel Timothy Oh <joeltimothyoh@live.com>
2022-10-10 10:25:16 +0000

    Debug shell variables with .sh file

---
+++ debug-shell-variables.sh
@@ -0,0 +1,15 @@
+BODY=$( echo '{}' | jq '
+{
+    "build_num": "$CIRCLE_BUILD_NUM",
+    "username": "$CIRCLE_PROJECT_USERNAME",
+    "reponame": "$CIRCLE_PROJECT_REPONAME",
+    "branch": "$CIRCLE_BRANCH",
+    "build_parameters": {
+        "var1": "$var1",
+        "var2": "$var2",
+        "var3": "$var3",
+        "var4": "$var4"
+    }
+}
+' )
+echo "BODY: $BODY"

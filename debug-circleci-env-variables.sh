2c42bd47fccaed530fcbb92bc0110895acef7258
Joel Timothy Oh <joeltimothyoh@live.com>
2022-10-10 10:37:26 +0000

    Debug jq vs cat interpolation interaction with circieci env vars in debug-circleci-env-variables.sh

--- debug-circleci-env-variables.sh
+++ debug-circleci-env-variables.sh
@@ -15,3 +15,20 @@ BODY=$( echo '{}' | jq '
 }
 ' )
 echo "BODY: $BODY"
+
+BODY=$( cat <<EOF
+{
+    "build_num": "$CIRCLE_BUILD_NUM",
+    "username": "$CIRCLE_PROJECT_USERNAME",
+    "reponame": "$CIRCLE_PROJECT_REPONAME",
+    "branch": "$CIRCLE_BRANCH",
+    "build_parameters": {
+        "var1": "$var1",
+        "var2": "$var2",
+        "var3": "$var3",
+        "var4": "$var4"
+    }
+}
+EOF
+)
+echo "BODY: $BODY"
